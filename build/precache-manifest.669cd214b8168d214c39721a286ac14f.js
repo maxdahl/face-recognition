c365d1ef8b9ae1025bda7921e171c813b1cf37c4
szeproductions <git@daagur.de>
2018-11-08 20:57:17 +0100

    publish on github

---
+++ build/precache-manifest.669cd214b8168d214c39721a286ac14f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff8a4ad9b5a6ad37d0182d9b8b89f2c9",
+    "url": "/static/media/logo.ff8a4ad9.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "00c63749de57ae2a52d4",
+    "url": "/static/js/main.00c63749.chunk.js"
+  },
+  {
+    "revision": "012a0feb757b7077032c",
+    "url": "/static/js/1.012a0feb.chunk.js"
+  },
+  {
+    "revision": "00c63749de57ae2a52d4",
+    "url": "/static/css/main.6e3b6b3c.chunk.css"
+  },
+  {
+    "revision": "012a0feb757b7077032c",
+    "url": "/static/css/1.de8cba5b.chunk.css"
+  },
+  {
+    "revision": "65d7dc23a16ccc8f8b41fe113f05abbb",
+    "url": "/index.html"
+  }
+];
